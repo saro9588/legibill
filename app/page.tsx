f9f7c9693cb4c264f883fc2e094200350b91a13f
SevanBadal <sgolnaza@gmail.com>
2023-11-24 01:11:33 -0500

    remove center styles

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
 	};
 
 	return (
-		<main className="flex min-h-screen flex-col items-center p-24">
+		<main className="flex min-h-screen flex-col p-24">
 			<input
 				type="text"
 				placeholder="Filter states..."
